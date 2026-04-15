8b84dc0ed80c457b5009f9e40686a372450dd9a4
ZachNobles <zachnobles04@gmail.com>
2026-04-15 18:52:19 -0400

    started writing publisher for leader follower thing

---
+++ engineers_leader_follower/engineers_leader_follower/path_planner.py
@@ -0,0 +1,72 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+import numpy as np
+
+
+class PathPlanner(Node):
+    def __init__(self):
+        super().__init__("path planner")
+        self.publisher_ = self.create_publisher(Twist, "robot1/raw_vel", 10)
+        self.publisher_ = self.create_publisher(Twist, "robot2/raw_vel", 10)
+
+        self.goal_points = [
+            (1.0, 5.0),
+            (1.0, 8.0),
+
+        ]
+
+        self.current_goal_point = self.goal_points[0]
+
+        self.robot1_goal = None
+        self.robot2_goal = None
+
+        self.robot1_points = []
+
+        self.robot1_x = 0.0
+        self.robot1_y = 0.0
+        self.robot1_theta = 0.0
+
+        self.velocity = 1.0
+        timer_period = 0.05 # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+        self.T_2_1 = np.array([0.0, -0.3, 0.0]).T # robot 2 to robot 1 transform
+    
+    def timer_callback(self):
+        msg = Twist()
+
+        self.robot1_points.append((self.robot1_x, self.robot1_y))
+
+        theta = np.arctan2(self.current_goal_point[1] - self.robot1_y, self.current_goal_point[0] - self.robot1_x)
+        msg.linear.x = self.velocity * np.cos(theta)
+        msg.linear.y = self.velocity * np.sin(theta)
+
+        if len(self.robot1_points) > 5:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    path_planner = PathPlanner()
+
+    rclpy.spin(path_planner)
+
+    path_planner.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- engineers_leader_follower/setup.py
+++ engineers_leader_follower/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'path_planner = engineers_leader_follower.path_planner:main',
         ],
     },
 )
