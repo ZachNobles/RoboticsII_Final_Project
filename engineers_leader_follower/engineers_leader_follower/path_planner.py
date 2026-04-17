701dac7be83bcf50c5aae2ad0a4bcc0aeb5850ca
ZachNobles <zachnobles04@gmail.com>
2026-04-16 23:35:27 -0400

    prototype

--- engineers_leader_follower/engineers_leader_follower/path_planner.py
+++ engineers_leader_follower/engineers_leader_follower/path_planner.py
@@ -8,8 +8,8 @@ import numpy as np
 class PathPlanner(Node):
     def __init__(self):
         super().__init__("path planner")
-        self.publisher_ = self.create_publisher(Twist, "robot1/raw_vel", 10)
-        self.publisher_ = self.create_publisher(Twist, "robot2/raw_vel", 10)
+        self.robot1_publisher = self.create_publisher(Twist, "robot1/raw_vel", 10)
+        self.robot2_publisher = self.create_publisher(Twist, "robot2/raw_vel", 10)
 
         self.goal_points = [
             (1.0, 5.0),
@@ -36,17 +36,23 @@ class PathPlanner(Node):
         self.T_2_1 = np.array([0.0, -0.3, 0.0]).T # robot 2 to robot 1 transform
     
     def timer_callback(self):
-        msg = Twist()
+        robot1_msg = Twist()
+        robot2_msg = Twist()
 
         self.robot1_points.append((self.robot1_x, self.robot1_y))
 
-        theta = np.arctan2(self.current_goal_point[1] - self.robot1_y, self.current_goal_point[0] - self.robot1_x)
-        msg.linear.x = self.velocity * np.cos(theta)
-        msg.linear.y = self.velocity * np.sin(theta)
+        theta1 = np.arctan2(self.current_goal_point[1] - self.robot1_y, self.current_goal_point[0] - self.robot1_x)
+        robot1_msg.linear.x = self.velocity * np.cos(theta1)
+        robot1_msg.linear.y = self.velocity * np.sin(theta1)
 
         if len(self.robot1_points) > 5:
-            pass
-
+            robot2_goal = self.robot1_points.pop(0)
+            theta2 = np.arctan2(robot2_goal[1] - self.robot2_y, robot2_goal[0] - self.robot2_x)
+            robot2_msg.linear.x = self.velocity * np.cos(theta2)
+            robot2_msg.linear.y = self.velocity * np.sin(theta2)
+        
+        self.robot1_publisher.publish(robot1_msg)
+        self.robot2_publisher.publish(robot2_msg)
 
 
 
